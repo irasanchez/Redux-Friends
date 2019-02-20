3f3308d7ac0e76ed37c6ea1547b812d2643972e2
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-19 21:24:20 -0700

    Implement basic file structure and initialState

---
+++ friends/src/reducers/friendsListReducer.js
@@ -0,0 +1,18 @@
+//import actions
+
+const initialState = {
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [],
+  error: null
+};
+
+export const friendsListReducer = (state = initialState, action) => {
+  return state;
+};
