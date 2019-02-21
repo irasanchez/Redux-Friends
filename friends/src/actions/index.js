65907936bc3d5623978118960eefa62dc59dbe53
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-20 23:00:58 -0700

    Add Boilerplate Post Action: addFriend & addFriendSuccess

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 export const FETCHING = "FETCHING";
 export const FAILURE = "FAILURE";
 export const SUCCESS = "SUCCESS";
+export const ADD_FRIEND = "ADD_FRIEND";
 
 export const getFriends = () => dispatch => {
   dispatch({ type: FETCHING });
@@ -20,4 +21,27 @@ export const getFriends = () => dispatch => {
         payload: error
       });
     });
+};
+
+export const addFriend = newFriend => dispatch => {
+  return axios
+    .post("http://localhost:5000/api/friends", newFriend)
+    .then(response => {
+      dispatch(addFriendSuccess(response.data));
+    })
+    .catch(error => {
+      throw error;
+    });
+};
+
+export const addFriendSuccess = data => {
+  return {
+    type: ADD_FRIEND,
+    payload: {
+      id: Math.random(),
+      name: data, //console.log out
+      age: data,
+      email: data
+    }
+  };
 };
