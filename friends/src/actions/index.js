d413c3ee962373c5bd72027bd7ad895a2e6aceea
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-20 21:08:16 -0700

    Implement API actions

---
+++ friends/src/actions/index.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+export const FETCHING = "FETCHING";
+export const FAILURE = "FAILURE";
+export const SUCCESS = "SUCCESS";
+
+export const getFriends = () => dispatch => {
+  dispatch({ type: FETCHING });
+  axios
+    .get('http://localhost:5000/api/friends')
+    .then(({data}) => {
+      dispatch({
+        type:SUCCESS, 
+        payload: data.results
+      })
+    .catch(error => {
+      dispatch({
+        type: FAILURE, 
+        payload: error
+      });
+    });
+    }
+};
